289b3658125f08538bc47830b835c08919f16367
Your Name <you@example.com>
2021-05-11 15:35:44 +0300

    code:show_histogtam_svg

--- main.cpp
+++ main.cpp
@@ -5,6 +5,59 @@ using namespace std;
 
 const size_t SCREEN_WIDTH = 80;
 const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
+void
+svg_begin(double width, double height) {
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+void
+svg_end() {
+    cout << "</svg>\n";
+}
+
+void
+svg_text(double left, double baseline, string text) {
+    cout << "<text x='" << left << "' y='" << baseline << "'>" << text << "</text>";
+}
+
+void svg_rect(double x, double y, double width, double height,
+    string stroke="black", string fill="blue") {
+    cout << "<rect x='" << x << "' y='" << y << "' width='" << width << "' height='" << height << "' stroke='" << stroke << "' fill='" << fill << "'/>";
+}
+
+void
+show_histogram_svg(const vector<size_t>& bins) {
+    const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    const auto BLOCK_WIDTH = 10;
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+   // svg_text(TEXT_LEFT, TEXT_BASELINE, to_string(bins[0]));
+   // svg_rect(TEXT_WIDTH,0, BLOCK_WIDTH, BIN_HEIGHT);
+    size_t max_bin = bins[0];
+    for (size_t j : bins)
+    {
+        if (max_bin < j)
+            max_bin = j;
+    }
+
+    double top = 0;
+    for (size_t bin : bins) {
+                const double bin_width = BLOCK_WIDTH * bin;
+                svg_text(TEXT_LEFT, top  + TEXT_BASELINE, to_string(bin));
+                svg_rect(TEXT_WIDTH, top,bin_width, BIN_HEIGHT);
+        top += BIN_HEIGHT;
+    }
+    svg_end();
+
+}
 
 
 vector<double>
@@ -100,6 +153,7 @@ int main() {
     cin >> bin_count;
 
     const auto bins = make_histogram(numbers, bin_count);
-    show_histogram_text(bins);
+    //show_histogram_text(bins);
+    show_histogram_svg(bins);
     return 0;
 }

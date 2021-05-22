3e8195f282fe47665263de85ed6587dde37a4249
Your Name <you@example.com>
2021-05-22 19:34:32 +0300

    отмена комита (Revert) "Форматный (printf)"
    This reverts commit 91ff45e7fc9321c1d0478749b1fa58ebf8a5ba98.

--- histogram.h
+++ histogram.h
@@ -3,8 +3,4 @@
 #include <vector>
 using namespace std;
 void find_minmax(vector<double> numbers, double& min, double& max) ;
-vector<size_t>
-make_histogram(const vector<double>& numbers, size_t bin_count);
-void
-show_histogram_text(const vector<size_t>& bins);
 #endif // HISTOGRAM_H_INCLUDED

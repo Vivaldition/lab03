7b82ef453c21191c4a03250669225b24ccc172cb
Your Name <you@example.com>
2021-05-22 15:28:07 +0300

    code: read_input(cin) and make_histogram(cin)

--- main.cpp
+++ main.cpp
@@ -9,6 +9,11 @@ using namespace std;
 const size_t SCREEN_WIDTH = 80;
 const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
 
+struct Input {
+    vector<double> numbers;
+    size_t bin_count;
+};
+
 vector<double>
 input_numbers(istream& in,size_t count) {
     vector<double> result(count);
@@ -18,17 +23,33 @@ input_numbers(istream& in,size_t count) {
     return result;
 }
 
+Input
+read_input(istream& in) {
+    Input data;
+
+    cerr << "Enter number count: ";
+    size_t number_count;
+    in >> number_count;
+
+    cerr << "Enter numbers: ";
+    data.numbers = input_numbers(in, number_count);
+
+     cerr << "Enter bin count: ";
+     size_t bin_count;
+     in >> bin_count;
+     return data;
+}
 
 vector<size_t>
-make_histogram(const vector<double>& numbers, size_t bin_count) {
-    vector<size_t> bins(bin_count);
+make_histogram(Input data) {
+    vector<size_t> bins(data.bin_count);
     double min, max, bin_size;
-    find_minmax(numbers, min, max);
-    bin_size = (max - min) / bin_count;
-    for (double i : numbers)
+    find_minmax(data.numbers, min, max);
+    bin_size = (max - min) / data.bin_count;
+    for (double i : data.numbers)
     {
         bool found = false;
-        for (size_t j = 0; (j < bin_count - 1) && !found; j++)
+        for (size_t j = 0; (j < data.bin_count - 1) && !found; j++)
         {
             auto lo = min + j * bin_size;
             auto hi = min + (j + 1) * bin_size;
@@ -40,7 +61,7 @@ make_histogram(const vector<double>& numbers, size_t bin_count) {
         }
         if (!found)
         {
-            bins[bin_count - 1]++;
+            bins[data.bin_count - 1]++;
         }
     }
     return bins;
@@ -80,13 +101,8 @@ show_histogram_text(const vector<size_t>& bins) {
 }
 
 int main() {
-    size_t number_count;
-    cin >> number_count;
-    const auto numbers = input_numbers(cin,number_count);
-    size_t bin_count;
-    cin >> bin_count;
-
-    const auto bins = make_histogram(numbers, bin_count);
+    const auto input= read_input(cin);
+    const auto bins = make_histogram(input);
     //show_histogram_text(bins);
     show_histogram_svg(bins);
     return 0;

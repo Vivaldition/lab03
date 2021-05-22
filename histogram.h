5d146a665c4b973709f17217c5feb648e29b2d53
Your Name <you@example.com>
2021-05-22 19:44:00 +0300

    printf GetVersion в 10 и 16 с.с

--- histogram.h
+++ histogram.h
@@ -3,4 +3,8 @@
 #include <vector>
 using namespace std;
 void find_minmax(vector<double> numbers, double& min, double& max) ;
+vector<size_t>
+make_histogram(const vector<double>& numbers, size_t bin_count);
+void
+show_histogram_text(const vector<size_t>& bins);
 #endif // HISTOGRAM_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include<sstream>
+#include <windows.h>
 
 #include "histogram.h"
 #include "svg.h"
 using namespace std;
 
-const size_t SCREEN_WIDTH = 80;
-const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
 
 vector<double>
 input_numbers(size_t count) {
@@ -18,68 +18,18 @@ input_numbers(size_t count) {
     return result;
 }
 
-
-vector<size_t>
-make_histogram(const vector<double>& numbers, size_t bin_count) {
-    vector<size_t> bins(bin_count);
-    double min, max, bin_size;
-    find_minmax(numbers, min, max);
-    bin_size = (max - min) / bin_count;
-    for (double i : numbers)
-    {
-        bool found = false;
-        for (size_t j = 0; (j < bin_count - 1) && !found; j++)
-        {
-            auto lo = min + j * bin_size;
-            auto hi = min + (j + 1) * bin_size;
-            if ((lo <= i) && (i < hi))
-            {
-                bins[j]++;
-                found = true;
-            }
-        }
-        if (!found)
-        {
-            bins[bin_count - 1]++;
-        }
-    }
-    return bins;
-}
-
-void
-show_histogram_text(const vector<size_t>& bins) {
-    size_t max_count = bins[0];
-    for (size_t j : bins)
-    {
-        if (max_count < j)
-            max_count = j;
-    }
-
-    for (size_t j : bins)
-    {
-        if (j < 10)
-            cout << "  " << j << "|";
-        else  if (j < 100 && j>10)
-            cout << " " << j << "|";
-        else if (j >= 100)
-            cout << j << "|";
-        if (max_count == j)
-        {
-            for (size_t i = 0; i < MAX_ASTERISK; i++)
-                cout << "*";
-        }
-        else
-        {
-            size_t height = MAX_ASTERISK * (static_cast<double>(j) / max_count);
-            for (size_t q = 0; q < height; q++)
-                cout << "*";
-        }
-        cout << endl;
-    }
-    return;
+string
+make_info_text() {
+    stringstream buffer;
+    printf("%u \n%x",GetVersion(),GetVersion());
+    // TODO: получить версию системы, записать в буфер.
+    // TODO: получить имя компьютера, записать в буфер.
+    return buffer.str();
 }
 
 int main() {
+    make_info_text();
+    /*
     size_t number_count;
     cin >> number_count;
     const auto numbers = input_numbers(number_count);
@@ -89,5 +39,6 @@ int main() {
     const auto bins = make_histogram(numbers, bin_count);
     //show_histogram_text(bins);
     show_histogram_svg(bins);
+    */
     return 0;
 }
